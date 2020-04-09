04c9538fc281fd522f176a4fe3dac4ffdfb1c0ee
Vaibhav Singh <sivaibhav@google.com>
2020-04-09 16:54:31 +0000

    local parallel working

--- contrib/model_parallel_mnist.py
+++ contrib/model_parallel_mnist.py
@@ -13,6 +13,18 @@ import torch_xla.distributed.xla_multiprocessing as xmp
 import torch_xla.utils.utils as xu
 from torchvision import datasets, transforms
 
+FLAGS = {}
+FLAGS['lr'] = 0.01
+FLAGS['datadir'] = "/tmp/mnist"
+FLAGS['batch_size'] = 128
+FLAGS['num_workers'] = 4
+FLAGS['learning_rate'] = 0.01
+FLAGS['momentum'] = 0.5
+FLAGS['num_epochs'] = 10
+FLAGS['num_cores'] = 8
+FLAGS['log_steps'] = 20
+FLAGS['metrics_debug'] = False
+FLAGS['tidy'] = False
 
 class MNIST(nn.Module):
 
@@ -38,65 +50,71 @@ class MNIST(nn.Module):
 
 class MNIST_model_parallel(MNIST):
   """ Model Parallel from existing model """
-  def __init__(self, gindex=0)
+  def __init__(self, gindex=0):
     super(MNIST_model_parallel, self).__init__()
     self.seq1 = nn.Sequential(
         self.conv1,
+        nn.MaxPool2d(2),
+        nn.ReLU(),
         self.bn1,
         self.conv2,
+        nn.MaxPool2d(2),
+        nn.ReLU(),
         self.bn2
     ).to(xm.xla_device(gindex))
     self.seq2 = nn.Sequential(
         self.fc1,
+        nn.ReLU(),
         self.fc2
     ).to(xm.xla_device(gindex+1))
     self.gindex = gindex
 
   def forward(self, x):
     x = self.seq1(x)
+    x = torch.flatten(x, 1)
     x = self.seq2(x.to(xm.xla_device(self.gindex + 1)))
     return F.log_softmax(x, dim=1)
 
 
 
 def train_mnist(gindex):
+  if gindex % 2 == 0:
+    return 0
+  device = xm.xla_device()
   train_loader = xu.SampleGenerator(
-      data=(torch.zeros(FLAGS.batch_size, 1, 28,
-                        28), torch.zeros(FLAGS.batch_size,
+      data=(torch.zeros(FLAGS['batch_size'], 1, 28,
+                        28), torch.zeros(FLAGS['batch_size'],
                                          dtype=torch.int64)),
-      sample_count=60000 // FLAGS.batch_size // xm.xrt_world_size())
+      sample_count=60000 // FLAGS['batch_size'] // xm.xrt_world_size())
   # Scale learning rate to num cores
-  lr = FLAGS.lr * xm.xrt_world_size()
+  lr = FLAGS['lr'] * xm.xrt_world_size()
   model = MNIST_model_parallel(gindex)
-  writer = None                   
- # if xm.is_master_ordinal():
- #   writer = test_utils.get_summary_writer(FLAGS.logdir)
-  optimizer = optim.SGD(model.parameters(), lr=lr, momentum=FLAGS.momentum)
+  optimizer = optim.SGD(model.parameters(), lr=lr, momentum=FLAGS['momentum'])
   loss_fn = nn.NLLLoss()
 
-  def train_loop_fn(loader):
+  def train_loop_fn(loader,gindex):
     tracker = xm.RateTracker()
 
     model.train()
     for step, (data, target) in enumerate(loader):
+      xm.master_print('Step-1 {} train begin'.format(step))
       optimizer.zero_grad()
       output = model(data)
+      target = target.to(xm.xla_device(gindex+1))
       loss = loss_fn(output, target)
       loss.backward()
       xm.optimizer_step(optimizer)
-      tracker.add(FLAGS.batch_size)
+      tracker.add(FLAGS['batch_size'])
 
-  for epoch in range(1, FLAGS.num_epochs + 1):
-    xm.master_print('Epoch {} train begin {}'.format(epoch, test_utils.now()))
+  for epoch in range(1, FLAGS['num_epochs'] + 1):
+    xm.master_print('Epoch {} train begin'.format(epoch))
     para_loader = pl.ParallelLoader(train_loader, [device])
-    train_loop_fn(para_loader.per_device_loader(device))
-    xm.master_print('Epoch {} train end {}'.format(epoch, test_utils.now()))
+    train_loop_fn(para_loader.per_device_loader(device),gindex)
+    xm.master_print('Epoch {} train end '.format(epoch))
 
-    para_loader = pl.ParallelLoader(test_loader, [device])
-    if FLAGS.metrics_debug:
+    if FLAGS['metrics_debug']:
       xm.master_print(met.metrics_report())
 
-  test_utils.close_summary_writer(writer)
   xm.master_print('Max Accuracy: {:.2f}%'.format(max_accuracy))
   return max_accuracy
 
@@ -105,9 +123,35 @@ def _mp_fn(index, flags):
   global FLAGS
   FLAGS = flags
   torch.set_default_tensor_type('torch.FloatTensor')
-  train_mnist(index)
-  if FLAGS.tidy and os.path.isdir(FLAGS.datadir):
-    shutil.rmtree(FLAGS.datadir)
+  train_mnist(index+1)
+  if FLAGS['tidy'] and os.path.isdir(FLAGS['datadir']):
+    shutil.rmtree(FLAGS['datadir'])
 
 if __name__ == '__main__':
-  xmp.spawn(_mp_fn, args=(FLAGS,), nprocs=FLAGS.num_cores)
+  #xmp.spawn(_mp_fn, args=(FLAGS,), nprocs=FLAGS['num_cores'])
+  #exit()
+  gindex = 7
+  device = xm.xla_device(gindex)
+  model = MNIST_model_parallel(gindex)
+  loss_fn = nn.NLLLoss()
+  
+  train_loader = xu.SampleGenerator(
+      data=(torch.zeros(FLAGS['batch_size'], 1, 28,
+                        28), torch.zeros(FLAGS['batch_size'],
+                                         dtype=torch.int64)),
+      sample_count=60000 // FLAGS['batch_size'] // xm.xrt_world_size())
+  para_loader = pl.ParallelLoader(train_loader, [device])
+  loader = para_loader.per_device_loader(device)
+
+  optimizer = optim.SGD(model.parameters(), lr=0.001)
+  optimizer.zero_grad()
+  tracker = xm.RateTracker()
+  model.train()
+  for step, (data, target) in enumerate(loader):
+    optimizer.zero_grad()
+    output = model(data)
+    target = target.to(xm.xla_device(gindex+1))
+    loss = loss_fn(output, target)
+    loss.backward()
+    xm.optimizer_step(optimizer)
+    tracker.add(FLAGS['batch_size'])
